343d2f51b5f0cd99ebd23a3f6b651186361de3b2
LerdoSeb <lerdo.sebastian@gmail.com>
2022-02-23 22:15:32 +0100

    Trial 1: Debugging val_fn

--- training.py
+++ training.py
@@ -13,7 +13,7 @@ plt.style.use(['science'])
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 32
-NUM_EPOCHS = 100             # 30
+NUM_EPOCHS = 10             # 30
 NUM_WORKERS = 4             # guideline: 4* num_GPU
 IMAGE_HEIGHT = 128          # 1280 originally
 IMAGE_WIDTH = 128           # 1918 originally
@@ -94,8 +94,8 @@ def val_fn(loader, model, loss_fn):
     loop = tqdm(loader)
 
     for batch_idx, (data, targets) in enumerate(loop):
-        data = data.float().unsqueeze(1).to(device=DEVICE)
-        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+        data = data.float().to(device=DEVICE)
+        targets = targets.float().to(device=DEVICE)
 
         with torch.cuda.amp.autocast():
             predictions = model(data)
