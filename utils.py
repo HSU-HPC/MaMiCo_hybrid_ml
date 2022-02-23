f341a77d570f12e677aa9cf253537ea602174516
LerdoSeb <lerdo.sebastian@gmail.com>
2022-02-23 23:53:45 +0100

    Trial 4: Debugging

--- utils.py
+++ utils.py
@@ -140,5 +140,5 @@ def save_predictions_as_imgs(loader, model, folder="saved_images/", device="cuda
     model.train()
 
 
-train_loader, val_loader, test_loader = get_loaders(
+# train_loader, val_loader, test_loader = get_loaders(
     32, 1, True, 31)
