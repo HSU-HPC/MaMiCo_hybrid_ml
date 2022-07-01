4267ebf4ba49a634858594c1d5df791345f7f4de
LerdoSeb <lerdo.sebastian@gmail.com>
2022-07-01 11:35:47 +0200

    Trial_1: Towards improving avg loss plots.

--- 3_Constituent_Hybrid_approach/plotting.py
+++ 3_Constituent_Hybrid_approach/plotting.py
@@ -263,7 +263,7 @@ def compareAvgLossRNN(l_of_l_files, l_of_l_labels, file_prefix=0, file_name=0):
 
         axs[i].set_xlabel('Number of Epochs')
         axs[i].set_ylabel('Error')
-        axs[i].legend(ncol=4, fontsize=10)
+        axs[i].legend(ncol=4, fontsize=8)
         axs[i].grid(axis='y')
 
     fig.set_size_inches(6, 10)
