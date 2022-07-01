0c5e9e2edf21d9a6ac6fdfa34679d5ebe2c3a57f
LerdoSeb <lerdo.sebastian@gmail.com>
2022-07-01 10:29:11 +0200

    Trial_1: Towards improving loss vs valid plots.

--- 3_Constituent_Hybrid_approach/plotting.py
+++ 3_Constituent_Hybrid_approach/plotting.py
@@ -266,6 +266,7 @@ def compareAvgLossRNN(l_of_l_files, l_of_l_labels, file_prefix=0, file_name=0):
         axs[i].set_ylabel('Error')
         axs[i].legend(ncol=1, fontsize=7)
 
+    plt.grid(axis='y')
     fig.set_size_inches(7, 10)
     if file_name != 0:
         fig.savefig(f'{file_prefix}Compare_Avg_Losses_{file_name}.svg')
