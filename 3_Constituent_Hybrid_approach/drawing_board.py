0f9d7118a3125ffa5fa698f08ec3bda279e375ec
LerdoSeb <lerdo.sebastian@gmail.com>
2022-07-04 23:59:35 +0200

    Trial_0: Towards generating validation error timeline.

--- 3_Constituent_Hybrid_approach/drawing_board.py
+++ 3_Constituent_Hybrid_approach/drawing_board.py
@@ -335,4 +335,27 @@ def trial_4_Hybrid_plots():
 
 
 if __name__ == "__main__":
-    trial_0_UNET_AE_plots()
+    _files = [
+        'Valid_Error_Timeline_C_3_0_B.csv',
+        'Valid_Error_Timeline_C_3_0_M.csv',
+        'Valid_Error_Timeline_C_3_0_T.csv',
+        'Valid_Error_Timeline_C_5_0_B.csv',
+        'Valid_Error_Timeline_C_5_0_M.csv',
+        'Valid_Error_Timeline_C_5_0_T.csv'
+    ]
+    _labels = [
+        'C 3 0 B',
+        'C 3 0 M',
+        'C 3 0 T',
+        'C 5 0 B',
+        'C 5 0 M',
+        'C 5 0 T',
+    ]
+    _directory = '/home/lerdo/lerdo_HPC_Lab_Project/MD_U-Net/3_Constituent_Hybrid_approach/Results/0_UNET_AE'
+    compareAvgLoss(
+        loss_files=_files,
+        loss_labels=_labels,
+        file_prefix=_directory,
+        file_name='valid_error_timeline'
+
+    )
