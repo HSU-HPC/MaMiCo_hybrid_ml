84d6cf7851a767bae89878c6665858d6a51c81b7
LerdoSeb <lerdo.sebastian@gmail.com>
2022-07-04 13:52:58 +0200

    Trial_0: Towards comparing flow profiles.

--- 3_Constituent_Hybrid_approach/plotting.py
+++ 3_Constituent_Hybrid_approach/plotting.py
@@ -460,14 +460,14 @@ def compareFlowProfile3x3(preds, targs, model_id='', dataset_id=''):
     for i in range(3):
         axs[i, 0].plot(steps, preds_avg[i][0], label='Averaged Prediction')
         axs[i, 0].plot(steps, targs_avg[i][0], label='Averaged Target')
-        axs[i, 0].set_yticks(list(np.arange(-1, 4, 0.5)))
+        axs[i, 0].set_yticks(list(np.arange(-0.5, 2.5, 0.5)))
         axs[i, 1].plot(steps, preds_avg[i][1], label='Averaged Prediction')
         axs[i, 1].plot(steps, targs_avg[i][1], label='Averaged Target')
-        axs[i, 1].set_yticks(list(np.arange(-0.5, 0.5, 0.5)))
+        axs[i, 1].set_yticks(list(np.arange(-0.25, 0.255, 0.25)))
         axs[i, 2].plot(steps, preds_avg[i][2], label='Averaged Prediction')
         axs[i, 2].plot(steps, targs_avg[i][2], label='Averaged Target')
-        axs[i, 2].set_yticks(list(np.arange(-0.5, 0.5, 0.5)))
-
+        axs[i, 2].set_yticks(list(np.arange(-0.25, 0.255, 0.25)))
+    axs.legend(ncol=1, fontsize=9)
     fig.savefig(
         f'CompareFlowprofile_{model_id}_{dataset_id}.svg')
     pass
