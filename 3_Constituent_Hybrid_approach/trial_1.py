13f6e19f795d7ec8cdbc1810ef41e04cbb4cbd39
LerdoSeb <lerdo.sebastian@gmail.com>
2022-07-14 11:31:37 +0200

    Housekeeping.

--- 3_Constituent_Hybrid_approach/trial_1.py
+++ 3_Constituent_Hybrid_approach/trial_1.py
@@ -281,7 +281,7 @@ def trial_1_UNET_AE(alpha, alpha_string, train_loaders, valid_loaders):
     _epoch_valids = []
 
     print('Beginning training.')
-    for epoch in range(50):
+    for epoch in range(3):
         _avg_loss = 0
         for _train_loader in train_loaders:
             _avg_loss += train_AE(
