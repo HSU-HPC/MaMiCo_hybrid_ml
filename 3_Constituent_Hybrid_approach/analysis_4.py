89ec9ad3c446655bb81ebb8ce71890910d1970f1
LerdoSeb <lerdo.sebastian@gmail.com>
2022-08-06 22:30:19 +0200

    Analysis_4: Towards evaluating ConvAE perf on random dataset w/ MSLE.

--- 3_Constituent_Hybrid_approach/analysis_4.py
+++ 3_Constituent_Hybrid_approach/analysis_4.py
@@ -74,6 +74,7 @@ def train_AE_MSLE(loader, model, optimizer, criterion, scaler, model_identifier,
         with torch.cuda.amp.autocast():
             _predictions = model(_data)
             _loss = criterion(_predictions.float(), _targets.float())
+            print(_loss, _loss.item())
             _epoch_loss += _loss
             _counter += 1
 
@@ -236,7 +237,7 @@ def analysis_4_KVS_non_UNET_mp():
     """
     print('Starting Analysis 4: AE (KVS)')
     _t_loaders, _v_loaders = get_UNET_AE_loaders(
-        data_distribution='get_KVS',
+        data_distribution='get_random',
         batch_size=8,
         shuffle=True
     )
