b19f3766af9f6243b0dd91507750c7ee05e21b94
LerdoSeb <lerdo.sebastian@gmail.com>
2023-01-12 09:10:22 -0500

    Towards deploying AE_u_i training. Here: Visualization of KVS eval.

--- 4_ICCS/trial_1.py
+++ 4_ICCS/trial_1.py
@@ -489,7 +489,7 @@ def prediction_retriever(model_directory, model_name, dataset_name, save2file_na
 
 
 if __name__ == "__main__":
-
+    '''
     print('Starting Trial 1: AE_u_i (KVS, MAE)')
     _alpha = 0.0001
     _alpha_string = '0_0001'
@@ -499,13 +499,12 @@ if __name__ == "__main__":
         shuffle=True
     )
     trial_1_AE(_alpha, _alpha_string, _train_loaders, _valid_loaders)
-
     '''
 
     print('Starting Trial 1: Prediction Retriever (KVS, MRE)')
 
-    _model_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/1_Conv_AE/kvs_05_mre'
-    _model_name = 'Model_AE_LR0_0001'
+    _model_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/1_Conv_AE/kvs_50_mae_u_i'
+    _model_name = 'Model_AE_u_i_LR0_0001'
     _dataset_name = 'get_KVS_eval'
     _save2file_name = 'pred_kvs_combined_domain_init_20000_NW'
 
@@ -515,4 +514,3 @@ if __name__ == "__main__":
         dataset_name=_dataset_name,
         save2file_name=_save2file_name
     )
-    '''
