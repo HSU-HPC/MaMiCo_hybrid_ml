f6a7e4e5773258c5c7b17f24804979d613e41d0f
LerdoSeb <lerdo.sebastian@gmail.com>
2023-01-17 22:37:27 +0100

    Towards deploying AE_u_i training. Here: Plotting LeakyReLU performance.

--- 4_ICCS/trial_1.py
+++ 4_ICCS/trial_1.py
@@ -866,7 +866,7 @@ def prediction_retriever_u_i(model_directory, model_name_x, model_name_y,
 
 
 if __name__ == "__main__":
-
+    '''
     print('Starting Trial 1: AE_u_i (KVS, MAE, LRLU)')
     _alpha = 0.0001
     _alpha_string = '0_0001'
@@ -878,14 +878,14 @@ if __name__ == "__main__":
     trial_1_AE_u_i(_alpha, _alpha_string, _train_loaders, _valid_loaders)
 
     '''
-    print('Starting Trial 1: Prediction Retriever (KVS, MRE, AE_u_i)')
+    print('Starting Trial 1: Prediction Retriever (KVS, MRE, LReLU, AE_u_i)')
 
-    _model_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/1_Conv_AE/kvs_50_mae_u_i'
+    _model_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/1_Conv_AE/kvs_50_mae_lrlu_u_i'
     _model_name_x = 'Model_AE_u_i_LR0_0001_x'
     _model_name_y = 'Model_AE_u_i_LR0_0001_y'
     _model_name_z = 'Model_AE_u_i_LR0_0001_z'
     _dataset_name = 'get_KVS_eval'
-    _save2file_name = 'pred_kvs_combined_domain_init_20000_NW'
+    _save2file_name = 'pred_lrelu_kvs_combined_domain_init_20000_NW'
 
     prediction_retriever_u_i(
         model_directory=_model_directory,
@@ -895,4 +895,3 @@ if __name__ == "__main__":
         dataset_name=_dataset_name,
         save2file_name=_save2file_name
     )
-    '''
