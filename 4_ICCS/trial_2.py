4f695a616862a91ba9d5bbf9a49783ff79c25e3d
LerdoSeb <lerdo.sebastian@gmail.com>
2023-01-23 23:59:37 +0100

    Towards visualizing RNN performance. Here: Piet loss function.

--- 4_ICCS/trial_2.py
+++ 4_ICCS/trial_2.py
@@ -1070,13 +1070,13 @@ def trial_2_RNN_single_verification():
     print('Starting Trial 2: Prediction Retriever (KVS + Aug, MAE, ReLU, AE_u_i, torch.add())')
 
     _model_AE_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/1_Conv_AE/kvs_aug_100_mae_relu_upshift/'
-    _model_RNN_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/2_RNN/kvs_aug_100_mae_single_RNN/'
+    _model_RNN_directory = '/beegfs/project/MaMiCo/mamico-ml/ICCS/MD_U-Net/4_ICCS/Results/2_RNN/kvs_aug_10_Piet_single_RNN/'
     _model_name_x = 'Model_AE_u_i_LR0_0001_x'
     _model_name_y = 'Model_AE_u_i_LR0_0001_y'
     _model_name_z = 'Model_AE_u_i_LR0_0001_z'
     _model_name_RNN = 'Model_RNN_LR1e-5_Lay1_Seq25_x'
     _dataset_name = 'get_KVS_eval'
-    _save2file_prefix = 'Model_100_relu_kvs_aug_upshift_Hybrid_RNN_Piet_3'
+    _save2file_prefix = 'Model_100_relu_kvs_aug_upshift_Hybrid_RNN_Piet_4'
     _save2file_name = '22000_NW_no_std'
 
     prediction_retriever_hybrid(
@@ -1094,6 +1094,7 @@ def trial_2_RNN_single_verification():
 
 if __name__ == "__main__":
     # trial_2_train_LSTM_single()
-    trial_2_train_RNN_single()
+    # trial_2_train_RNN_single()
+    trial_2_RNN_single_verification()
 
     pass
