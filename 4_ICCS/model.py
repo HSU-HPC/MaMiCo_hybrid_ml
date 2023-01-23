f385be9a75b95f635f835f3bf3007ac7ce514561
LerdoSeb <lerdo.sebastian@gmail.com>
2023-01-23 22:32:10 +0100

    Towards visualizing RNN-Hybrid outputs w/ Piet.

--- 4_ICCS/model.py
+++ 4_ICCS/model.py
@@ -986,15 +986,15 @@ class Hybrid_MD_RNN_AE_u_i(nn.Module):
             self.sequence_y, (1, self.seq_length, 256)).to(self.device)
         interim_z = torch.reshape(
             self.sequence_z, (1, self.seq_length, 256)).to(self.device)
-        
+
         u_x = self.rnn_x(interim_x).to(self.device)
         u_y = self.rnn_y(interim_y).to(self.device)
         u_z = self.rnn_z(interim_z).to(self.device)
-
+        '''
         u_x = interim_x[0, -1, :]
         u_y = interim_y[0, -1, :]
         u_z = interim_z[0, -1, :]
-
+        '''
         u_x = torch.reshape(u_x, u_x_shape).to(self.device)
         u_y = torch.reshape(u_y, u_y_shape).to(self.device)
         u_z = torch.reshape(u_z, u_z_shape).to(self.device)
