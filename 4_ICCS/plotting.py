195776f5f51d5901c0e4d7366c108fd1cd117cf5
LerdoSeb <lerdo.sebastian@gmail.com>
2023-02-27 13:03:58 +0100

    Towards finalizing figs [:] for paper.

--- 4_ICCS/plotting.py
+++ 4_ICCS/plotting.py
@@ -123,13 +123,16 @@ def plot_flow_profile_std(np_datasets, dataset_legends, save2file):
 
     axs[0].set_ylabel(r'$\mathbf{u_x}$', fontsize=12, fontweight='bold')
     axs[0].grid(axis='y', alpha=0.3)
+    axs[0].set_ylim([3.0, 6.5])
 
     axs[1].set_ylabel(r'$\mathbf{u_y}$', fontsize=12, fontweight='bold')
     axs[1].grid(axis='y', alpha=0.3)
+    axs[1].set_ylim([-1.0, 1.0])
 
     axs[2].set_xlabel("t", fontsize=12, fontweight='bold')
     axs[2].set_ylabel(r'$\mathbf{u_z}$', fontsize=12, fontweight='bold')
     axs[2].grid(axis='y', alpha=0.3)
+    axs[2].set_ylim([-1.0, 1.0])
 
     for idx, dataset in enumerate(np_datasets):
         alpha = 0.4 * 2
